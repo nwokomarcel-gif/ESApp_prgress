ad00cd94a5b5375cfbba804c83bde7f2ea4ed625
Marcel <nwokomarcel@gmail.com>
2026-05-21 00:44:19 +0100

    remove node_modules from tracking

--- ESapp_progress_v2/vite.config.js
+++ ESapp_progress_v2/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ESApp_progress_v2/
+  base: '/ESApp_progress_v2/',
 })
